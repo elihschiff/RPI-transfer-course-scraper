e55b7054f271be2b6a9e98148df9c1c62b19d6b0
Eli Schiff <eli@elischiff.org>
2019-12-12 15:52:50 -0500

    python black and runs the same code up to 10 times if it crashes

--- scrape.py
+++ scrape.py
@@ -5,9 +5,12 @@ from bs4 import BeautifulSoup
 import csv
 import sys
 import re
+import time
 
 
-course_id = input("Enter the course id exactly as you see it on sis EX:'CSCI 1200' (capitalization is important): ").strip()
+course_id = input(
+    "Enter the course id exactly as you see it on sis EX:'CSCI 1200' (capitalization is important): "
+).strip()
 print(course_id)
 output_file_name = input("Enter the output file name: ").strip()
 print(output_file_name)
@@ -26,72 +29,96 @@ output_data = []
 
 def scrape_place(type):
     states_select = driver.find_element_by_name(type)
-    states = [x.get_attribute('value') for x in states_select.find_elements_by_tag_name("option")]
-    states_real_names = [x.text for x in states_select.find_elements_by_tag_name("option")]
+    states = [
+        x.get_attribute("value")
+        for x in states_select.find_elements_by_tag_name("option")
+    ]
+    states_real_names = [
+        x.text for x in states_select.find_elements_by_tag_name("option")
+    ]
 
     for i in range(len(states)):
-        if(states[i] == ""):
+        if states[i] == "":
             continue
 
         print("State/Nation:", states[i])
-        Select(driver.find_element_by_name(type)).select_by_value(states[i]);#set the select box to the current state
-        driver.find_element_by_css_selector("[value='Get Institutions']").click()#click button to load schools
+        Select(driver.find_element_by_name(type)).select_by_value(states[i])
+        # set the select box to the current state
+        driver.find_element_by_css_selector(
+            "[value='Get Institutions']"
+        ).click()  # click button to load schools
 
-        #get list of schools for this state
-        schools_select = driver.find_element_by_name('sbgi_code')
-        schools = [x.get_attribute('value') for x in schools_select.find_elements_by_tag_name("option")]
+        # get list of schools for this state
+        schools_select = driver.find_element_by_name("sbgi_code")
+        schools = [
+            x.get_attribute("value")
+            for x in schools_select.find_elements_by_tag_name("option")
+        ]
 
-        scrape_school(schools,states[i],type,states_real_names[i])
+        scrape_school(schools, states[i], type, states_real_names[i])
 
-        driver.get(transfer_course_url)    #go back to main page for next state
-
-
+        driver.get(transfer_course_url)  # go back to main page for next state
 
 
 def save_data():
-    with open(output_file_name, 'w', newline='') as f:
+    with open(output_file_name, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(output_data)
 
 
 def scrape(real_name):
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    school_name = soup.find("caption", {"class": "captiontext"}).find("strong").text
+    soup = BeautifulSoup(driver.page_source, "html.parser")
 
+    school_name = None
+    for i in range(0, 10):  # tries this up to 10 times if it crashes
+        try:
+            school_name = (
+                soup.find("caption", {"class": "captiontext"}).find("strong").text
+            )
+        except:
+            time.sleep(5)
+            continue
+        break
 
     table = soup.find("table", {"id": "TransArtTable"})
     table = table.find("tbody")
-    rows = table.findAll('tr')
+    rows = table.findAll("tr")
     for i in range(len(rows)):
-        cells = rows[i].findAll('td')
-        if(len(cells) > 3 ):
-            if(cells[3].text.strip() == course_id):
+        cells = rows[i].findAll("td")
+        if len(cells) > 3:
+            if cells[3].text.strip() == course_id:
                 print("Found class at", school_name)
-                #gets the offsets because some data actually fills more than one row
-                row_offset_up=0
-                while(len(rows[i-row_offset_up].findAll('td'))>1):
+                # gets the offsets because some data actually fills more than one row
+                row_offset_up = 0
+                while len(rows[i - row_offset_up].findAll("td")) > 1:
                     row_offset_up += 1
-                row_offset_down=0
-                while(len(rows[i+row_offset_down].findAll('td'))>1):
+                row_offset_down = 0
+                while len(rows[i + row_offset_down].findAll("td")) > 1:
                     row_offset_down += 1
 
                 # print(school_name, cells[3].text, row_offset_up, row_offset_down)
 
-                #scrape data between the offsets
+                # scrape data between the offsets
                 start_cell_offset = 2
 
-                output_line = [""] * (5+start_cell_offset)
-                output_line[0]=real_name
+                output_line = [""] * (5 + start_cell_offset)
+                output_line[0] = real_name
                 output_line[1] = school_name
-                for offset in range(i-row_offset_up+1, i+row_offset_down):
+                for offset in range(i - row_offset_up + 1, i + row_offset_down):
                     # print(offset)
-                    current_cells = rows[offset].findAll('td')
-                    for j in range(start_cell_offset,min(len(current_cells)+start_cell_offset-1, (5+start_cell_offset))):
-                        temp = current_cells[j-start_cell_offset+1].text.strip()
-                        if(temp != ""):
-                            output_line[j] += re.sub(' +', ' ', temp) + "\n"
+                    current_cells = rows[offset].findAll("td")
+                    for j in range(
+                        start_cell_offset,
+                        min(
+                            len(current_cells) + start_cell_offset - 1,
+                            (5 + start_cell_offset),
+                        ),
+                    ):
+                        temp = current_cells[j - start_cell_offset + 1].text.strip()
+                        if temp != "":
+                            output_line[j] += re.sub(" +", " ", temp) + "\n"
 
-                #remove extra \n at the end of a cell
+                # remove extra \n at the end of a cell
                 for j in range(len(output_line)):
                     output_line[j] = output_line[j].strip()
 
@@ -102,25 +129,32 @@ def scrape(real_name):
     # print(output_data)
 
 
-
-#schools is the list of schoolsys.argv[s
-#place is the state or the nation
-#type is "stat_code" for a state or "natn_code" for a nation
+# schools is the list of schoolsys.argv[s
+# place is the state or the nation
+# type is "stat_code" for a state or "natn_code" for a nation
 def scrape_school(schools, place, type, real_name):
     for school in schools:
-        if(school == ""):
+        if school == "":
             continue
         # print("Working on school number:", school)
-        Select(driver.find_element_by_name('sbgi_code')).select_by_value(school)#set the select box to the current school
-        driver.find_element_by_css_selector("[value='Get Courses']").click()#click to load the classes for the course
+        Select(driver.find_element_by_name("sbgi_code")).select_by_value(
+            school
+        )  # set the select box to the current school
+        driver.find_element_by_css_selector(
+            "[value='Get Courses']"
+        ).click()  # click to load the classes for the course
 
         scrape(real_name)
 
-        driver.get(transfer_course_url)#go back to main page for next college
+        driver.get(transfer_course_url)  # go back to main page for next college
 
-        Select(driver.find_element_by_name(type)).select_by_value(place);#repick the state
-        select_button = driver.find_element_by_css_selector("[value='Get Institutions']").click()#reclick Get Institutions button
+        Select(driver.find_element_by_name(type)).select_by_value(place)
+        # repick the state
+        select_button = driver.find_element_by_css_selector(
+            "[value='Get Institutions']"
+        ).click()  # reclick Get Institutions button
 
-#scrape schools then nations
-scrape_place('stat_code')
-scrape_place('natn_code')
+
+# scrape schools then nations
+scrape_place("stat_code")
+scrape_place("natn_code")
